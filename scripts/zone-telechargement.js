7b81a8349cc6816f360d89627d6e3b3bda87e357
hochgenug <hochgenug@gmail.com>
2017-10-10 23:27:13 +0200

    [ZT] Display lang & resolution on search page + add auto update

--- scripts/zone-telechargement.js
+++ scripts/zone-telechargement.js
@@ -1,5 +1,6 @@
+// Auto search when the param search is set
 if ($('#searchinput').length === 0) {
-    $.urlParam = function(name) {
+    $.urlParam = function (name) {
         var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
         return results[1] || 0;
     };
@@ -8,3 +9,32 @@ if ($('#searchinput').length === 0) {
     $('#searchform .submit input').trigger('click');
     eval(event);
 }
+
+// Display language and resolution
+$('.cover_global').each(function () {
+    // Increase height in order ton add one more line with details
+    $(this).css({height: "248px"});
+
+    var serieLink = $(this).children('div').children('a').attr('href');
+    // Extract data from the link
+    var resolution = serieLink.match(/\d{3,4}(?:p|i)/i);
+    var language = serieLink.match(/(?:french|vostfr|multi)/i);
+
+    var details = '';
+    if (resolution !== null) {
+        details += resolution + ' ';
+
+        // Highlight the HD files
+        if (resolution == "1080p") {
+            $(this).css({border: "2px #ff5d00 solid", padding: "0px", "box-shadow": "10px 10px 5px rgba(232, 187, 23, 0.66)"});
+        } else {
+            $(this).css({border: "2px #e4a6af solid", padding: "0px", "box-shadow": "10px 10px 5px #888"});
+        }
+    }
+    if (language !== null) {
+        details += language;
+    }
+    if (details !== null) {
+        $(this).find('.cover_infos_title').children('a').append("<div>" + details.toUpperCase() + "</div>");
+    }
+});
