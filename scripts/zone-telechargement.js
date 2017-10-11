9460b48a722efe1d0443cf47211f05d6bff76987
hochgenug <hochgenug@gmail.com>
2017-10-11 22:51:04 +0200

    [ZT] Allow to display the resolution with format hdXXX

--- scripts/zone-telechargement.js
+++ scripts/zone-telechargement.js
@@ -17,15 +17,18 @@ $('.cover_global').each(function () {
 
     var serieLink = $(this).children('div').children('a').attr('href');
     // Extract data from the link
-    var resolution = serieLink.match(/\d{3,4}(?:p|i)/i);
+    var resolution = serieLink.match(/(\d{3,4}(?:p|i)|(?:hd)\d{3,4})/i);
     var language = serieLink.match(/(?:french|vostfr|multi)/i);
 
     var details = '';
     if (resolution !== null) {
-        details += resolution + ' ';
+        if(resolution[0].indexOf('hd') !== -1){
+            resolution[0] = resolution[0].replace('hd','') + 'p';
+        }
+        details += resolution[0] + ' ';
 
         // Highlight the HD files
-        if (resolution == "1080p") {
+        if (resolution[0] == "1080p") {
             $(this).css({border: "2px #ff5d00 solid", padding: "0px", "box-shadow": "10px 10px 5px rgba(232, 187, 23, 0.66)"});
         } else {
             $(this).css({border: "2px #e4a6af solid", padding: "0px", "box-shadow": "10px 10px 5px #888"});
