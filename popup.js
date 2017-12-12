e89e8e3c02945268499d11a41d45343858468b1c
hochgenug <hochgenug@gmail.com>
2017-12-12 22:50:05 +0100

    Add the possibilty to handle the url for the protected links

---
+++ popup.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', documentEvents  , false);
+
+function myAction(input) {
+    localStorage.setItem("url",  input.value);
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {greeting:  localStorage.getItem("url")}, function(response) {});
+    });
+
+
+}
+
+function documentEvents() {
+
+    document.getElementById("name_textbox").value = localStorage.getItem("url");
+
+    document.getElementById('ok_btn').addEventListener('click',
+        function() { myAction(document.getElementById('name_textbox'));
+        });
+
+
+}
