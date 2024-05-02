a301ef0484d7147e5571d392c94fc619d6f2f5e0
Hochgenug <hochgenug@gmail.com>
2024-05-02 20:28:24 +0200

    Add debrid

--- popup.js
+++ popup.js
@@ -14,19 +14,13 @@ function documentEvents() {
         function (element) {
             chrome.storage.sync.set({"isEdEnable": element.srcElement.checked});
         });
-
-    document.getElementById("utb-feature").addEventListener("click",
-        function (element) {
-            chrome.storage.sync.set({"isUtbEnable": element.srcElement.checked});
-        });
 }
 
 let promise = new Promise(function (resolve) {
     chrome.storage.sync.get({
         isZtZaEnable: true,
         isBsEnable: true,
-        isEdEnable: true,
-        isUtbEnable: true,
+        isEdEnable: true
     }, function (items) {
         resolve(items);
     });
@@ -35,7 +29,6 @@ promise.then(function (items) {
     document.getElementById("ztza-feature").checked = items.isZtZaEnable;
     document.getElementById("bs-feature").checked = items.isBsEnable;
     document.getElementById("ed-feature").checked = items.isEdEnable;
-    document.getElementById("utb-feature").checked = items.isUtbEnable;
 });
 
 document.addEventListener("DOMContentLoaded", documentEvents, false);
